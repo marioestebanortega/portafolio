a66b58f9c847ad3932ecc8e1d9e8384d276dcec2
Proyecto ABBA <mario.esteban.ortega@gmail.com>
2020-07-06 21:11:20 -0500

    Portafolio responsive detalles

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,31 @@
 
 (() => {
+    const menu = document.querySelector('.menu');    
+    const body = document.querySelector('body');
+
+    const closeMenu=()=>{
+        if(menu.classList.contains('is-active')){
+           menu.classList.remove('is-active');
+        }
+    }
+
     const ipad = window.matchMedia('screen and (max-width: 767px)');
 
         
-        const menu = document.querySelector('.menu');
+       
         const burgerMenu = document.querySelector('#burger-menu');
         const hideShow = () => {
-            console.log('Se ejecuto');
+            
+                body.addEventListener('click',()=>{});
+            
+           
             if (menu.classList.contains('is-active')) {
                 menu.classList.remove('is-active');
 
             }
             else {
                 menu.classList.add('is-active');
+                body.addEventListener('click',closeMenu,true);
             }
 
         }
